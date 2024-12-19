f22557655ca4f0bc6b27a372fdb6da4514d85a02
Gambarenerd <dmarcoaldi@Danieles-MacBook-Pro.local>
2024-12-19 16:10:56 +0100

    Translation of the segment, issue with editing of tra_am

--- src/app/translation/translation.component.ts
+++ src/app/translation/translation.component.ts
@@ -7,139 +7,174 @@ import { CommonModule } from '@angular/common';
 import { ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import {MatToolbar} from "@angular/material/toolbar";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @Component({
- selector: 'app-translation',
- standalone: true,
- imports: [MatCardModule, FormsModule, CommonModule, MatGridListModule, MatButtonModule, MatInputModule, MatToolbar, MatTabGroup, MatTab, MatIconModule],
- templateUrl: './translation.component.html',
- styleUrls: ['./translation.component.css'],
- encapsulation: ViewEncapsulation.None
+  selector: 'app-translation',
+  standalone: true,
+  imports: [MatCardModule, FormsModule, CommonModule, MatGridListModule, MatButtonModule, MatInputModule, MatToolbar, MatTabGroup, MatTab, MatIconModule, MatProgressSpinnerModule],
+  templateUrl: './translation.component.html',
+  styleUrls: ['./translation.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TranslationComponent {
- originalText: string = '';
- amendedText: string = '';
- synonyms: string[] = [];
- originalSegments: string[] = [];
- amendedSegments: string[] = [];
- firstTabContent: string = '';
- amendments: { segmented_original: string[]; segmented_amended: string[] }[] = [];
- currentPageIndex: number = 0;
+  synonyms: string[] = [];
 
- constructor(private http: HttpClient) {}
+  originalSegments: string[] = [];
+  amendedSegments: string[] = [];
+  translatedSegments: string[] = [];
+  translatedAmendedSegments: string[] = [];
 
- translateText(segment: string) {
-   const backendUrl = 'http://127.0.0.1:8000/translate';
-   this.http.post<any>(backendUrl, { text: segment }).subscribe(
-     (response) => {
-       this.firstTabContent = response.translated_text;
-     },
-     (error) => {
-       console.error('Errore durante la traduzione:', error);
-     }
-   );
- }
+  firstTabContent: string = '';
+  amendments: { seg_ori: string[]; seg_am_ori: string[]; seg_tra: string[]; seg_am_tra: string[] }[] = [];
+  currentPageIndex: number = 0;
 
- // Keep it for later
- highlightWord(event: MouseEvent) {
-   const selection = window.getSelection();
-   const selectedWord = selection ? selection.toString().trim() : '';
+  isExpanded: boolean = false;
+  isEditing: boolean = false;
 
-   if (selectedWord) {
-     const backendUrl = 'http://127.0.0.1:8000/synonyms'; // Modifica con il tuo endpoint
-     this.http.post<any>(backendUrl, { word: selectedWord }).subscribe(
-       (response) => {
-         this.synonyms = response.synonyms || [];
-       },
-       (error) => {
-         console.error('Errore durante il recupero dei sinonimi:', error);
-       }
-     );
-   }
- }
+  isLoading: boolean = false; // Stato del loader
 
- onFileUpload(event: Event): void {
-  const input = event.target as HTMLInputElement;
+  constructor(private http: HttpClient) { }
 
-  if (input.files && input.files.length > 0) {
-    const file = input.files[0];
-    const formData = new FormData();
-    formData.append('file', file);
-
-    // Invia il file al back-end
-    const backendUrl = 'http://127.0.0.1:8000/upload/';
-    this.http.post<any>(backendUrl, formData).subscribe(
+  translateText(segment: string) {
+    const backendUrl = 'http://127.0.0.1:8000/translate';
+    this.http.post<any>(backendUrl, { text: segment }).subscribe(
       (response) => {
-        console.log('Risultato Analisi:', response);
-        // Puoi aggiornare il testo nella UI
-        this.amendments = response || [];
-        this.currentPageIndex = 0;
-        this.updatePage()
+        this.firstTabContent = response.translated_text;
       },
       (error) => {
-        console.error('Errore durante l\'upload del file:', error);
+        console.error('Errore durante la traduzione:', error);
       }
     );
   }
-}
 
-updatePage(): void {
-  if (this.amendments.length > 0 && this.currentPageIndex < this.amendments.length) {
-    const wrappers = document.querySelectorAll('.page-wrapper') as NodeListOf<HTMLElement>;
 
-    wrappers.forEach((wrapper) => {
-      wrapper.classList.add('animate');
+  // Keep it for later
+  highlightWord(event: MouseEvent) {
+    const selection = window.getSelection();
+    const selectedWord = selection ? selection.toString().trim() : '';
 
-      setTimeout(() => {
-        wrapper.classList.remove('animate');
 
-        
-        if (wrapper.classList.contains('original')) {
-          this.originalSegments = this.amendments[this.currentPageIndex].segmented_original;
-        } else if (wrapper.classList.contains('amended')) {
-          this.amendedSegments = this.amendments[this.currentPageIndex].segmented_amended;
+    if (selectedWord) {
+      const backendUrl = 'http://127.0.0.1:8000/synonyms'; // Modifica con il tuo endpoint
+      this.http.post<any>(backendUrl, { word: selectedWord }).subscribe(
+        (response) => {
+          this.synonyms = response.synonyms || [];
+        },
+        (error) => {
+          console.error('Errore durante il recupero dei sinonimi:', error);
         }
-      }, 300);
-    });
+      );
+    }
   }
-}
 
 
-nextPage(): void {
-  if (this.currentPageIndex < this.amendments.length - 1) {
-    this.currentPageIndex++;
-    this.updatePage();
+  onFileUpload(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const formData = new FormData();
+      formData.append('file', file);
+
+      this.isLoading = true;
+
+      const backendUrl = 'http://127.0.0.1:8000/upload/';
+      this.http.post<any>(backendUrl, formData).subscribe(
+        (response) => {
+          console.log('Risultato Analisi:', response);
+
+          this.amendments = response || [];
+          this.currentPageIndex = 0;
+          this.updatePage()
+          this.isLoading = false;
+        },
+        (error) => {
+          console.error('Errore durante l\'upload del file:', error);
+        }
+      );
+    }
   }
-}
 
-previousPage(): void {
-  if (this.currentPageIndex > 0) {
-    this.currentPageIndex--;
-    this.updatePage();
+
+  updatePage(): void {
+    if (this.amendments.length > 0 && this.currentPageIndex < this.amendments.length) {
+      const wrappers = document.querySelectorAll('.page-wrapper') as NodeListOf<HTMLElement>;
+
+
+      wrappers.forEach((wrapper) => {
+        wrapper.classList.add('animate');
+
+
+        setTimeout(() => {
+          wrapper.classList.remove('animate');
+
+
+          if (wrapper.classList.contains('original')) {
+            this.originalSegments = this.amendments[this.currentPageIndex].seg_ori;
+          } else if (wrapper.classList.contains('amended')) {
+            this.amendedSegments = this.amendments[this.currentPageIndex].seg_am_ori;
+          } else if (wrapper.classList.contains('translated')) {
+            this.translatedSegments = this.amendments[this.currentPageIndex].seg_tra;
+          } else if (wrapper.classList.contains('translated_amended')) {
+            this.translatedAmendedSegments = this.amendments[this.currentPageIndex].seg_am_tra;
+          }
+        }, 300);
+      });
+    }
   }
-}
 
-highlightSegment(event: MouseEvent): void {
-  const target = event.target as HTMLElement;
-  if (target) {
-    target.style.backgroundColor = '#e4aa3e';
+
+  nextPage(): void {
+    if (this.currentPageIndex < this.amendments.length - 1) {
+      this.currentPageIndex++;
+      this.updatePage();
+    }
   }
-}
 
-clearHighlight(event: MouseEvent): void {
-  const target = event.target as HTMLElement;
-  if (target) {
-    target.style.backgroundColor = '';
+
+  previousPage(): void {
+    if (this.currentPageIndex > 0) {
+      this.currentPageIndex--;
+      this.updatePage();
+    }
   }
-}
 
-processSegment(segment: string): void {
-  console.log('Segmento selezionato:', segment);
-  this.translateText(segment)
-}
+
+  highlightSegment(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (target) {
+      target.style.backgroundColor = '#e4aa3e';
+    }
+  }
+
+
+  clearHighlight(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (target) {
+      target.style.backgroundColor = '';
+    }
+  }
+
+
+  processSegment(segment: string): void {
+    console.log('Segmento selezionato:', segment);
+    this.translateText(segment)
+  }
+
+
+  // Toggle per espandere/ridurre e attivare/disattivare editing
+  toggleExpand(): void {
+    this.isEditing = !this.isEditing;
+  }
+
+  saveSegment(index: number) {
+    // Logica opzionale per salvare il segmento modificato
+    console.log('Salvato segmento', index, this.translatedAmendedSegments[index]);
+  }
 
 }
