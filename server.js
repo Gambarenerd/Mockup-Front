e04d13878d7529a3a2a4ac0496e411147c04ff2b
Gambarenerd <dmarcoaldi@Danieles-MacBook-Pro.local>
2024-12-19 17:06:42 +0100

    Adapting server.js file

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // Serve i file statici della build Angular
-app.use(express.static(path.join(__dirname, 'dist/translation-app')));
+app.use(express.static(path.join(__dirname, 'dist/translation-app/browser')));
 
 // Gestione fallback per le Single Page Application
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/translation-app/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/translation-app/browser/index.html'));
 });
 
 app.listen(port, () => {
